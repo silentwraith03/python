d637f379ed01881df1e36c77a737502a9284ef65
Kunal Pal <52573140+silentwraith03@users.noreply.github.com>
2022-10-29 11:28:20 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+# list = []
+# print("Blank list: ")
+# print(list)
+
+# list = [10,20,30]
+# print("List of numbers: ")
+# print(list)
+
+# list = ["Name","is","Kunal"]
+# print(list[1])
+# print(list[1])
+# print(list[-2])
+
+# string = input("Enter elements (Space-Seperated): ")
+# lst = string.split("_")
+# print("The list is",lst)
+
+lst = [10,20,30,40,50,"yash",70,"ravi",90,100]
+print(lst[::2])
